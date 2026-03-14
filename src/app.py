db60f7d67d65886d6ebf858851358f61f5e44230
Sergio Hernández <sohernandezo@yahoo.com.mx>
2026-03-14 03:12:34 +0000

    Spotipy SOHO

--- src/app.py
+++ src/app.py
@@ -2,9 +2,31 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 from dotenv import load_dotenv
+import seaborn as sns
+import spotipy
+import spotipy as spotify
 
 # load the .env file variables
 load_dotenv()
 
+# Get credential values
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+print(f'El ID es: {client_id}')
 
-# Spotify API credentials
+# Get credential values
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+
+id_artista = '2DaxqgrOhkeH0fpeiQq2f4'
+
+results = spotipy.Spotify.artist_top_tracks(self, artist_id=id_artista)
+canciones = []
+
+for track in results['tracks']:
+    canciones.append(
+        {'name':track['name']},
+        {'popularity' : track['popularity']}
+    )
+
+print(track)
